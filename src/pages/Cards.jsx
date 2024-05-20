9454ef74c0faa89c6635d73bb856c2d27f86f740
Lucas Madrigal <lucasmadrigalb@gmail.com>
2024-05-20 00:10:59 -0300

    cards view done

--- src/pages/Cards.jsx
+++ src/pages/Cards.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
-
+import React from "react";
+import '../styles/Cards.css'
+import BlackCard from '../assets/images/imgs/black_card.png'
+import GoldCard from '../assets/images/imgs/gold_card.png'
+import PlatinumCard from '../assets/images/imgs/platinum_card.png'
 const Cards = () => {
   return (
-    <div>Cards</div>
-  )
-}
+    <>
+      <h1>Your cards</h1>
+      <div className="main-cards--container">
+        <h2>Credit</h2>
+        <div className="cards--container">
+          <img src={BlackCard} alt="" />
+          <img src={GoldCard} alt="" />
+          <img src={PlatinumCard} alt="" />
+        </div>
+        <h2>Debit</h2>
+        <div className="cards--container">
+          <img src={BlackCard} alt="" />
+          <img src={GoldCard} alt="" />
+          <img src={PlatinumCard} alt="" />
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Cards
+export default Cards;
