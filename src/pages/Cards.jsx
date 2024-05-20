83e5f406d48cea6375fca5c61ddf65c4d54fad32
Lucas Madrigal <lucasmadrigalb@gmail.com>
2024-05-20 19:55:25 -0300

    vista de cards wip

--- src/pages/Cards.jsx
+++ src/pages/Cards.jsx
@@ -4,6 +4,7 @@ import BlackCard from "../assets/images/imgs/black_card.png";
 import GoldCard from "../assets/images/imgs/gold_card.png";
 import PlatinumCard from "../assets/images/imgs/platinum_card.png";
 import { NavLink } from "react-router-dom";
+import CardsElement from "../components/CardsElement";
 const Cards = () => {
   return (
     <>
@@ -11,21 +12,21 @@ const Cards = () => {
       <div className="main-cards--container">
         <h2>Credit</h2>
         <div className="cards--container">
-          <img src={BlackCard} alt="" />
-          <img src={GoldCard} alt="" />
-          <img src={PlatinumCard} alt="" />
+          <CardsElement card={"GOLD"} />
+          <CardsElement card={"TITANIUM"} />
+          <CardsElement card={"BLACK"} />
         </div>
         <h2>Debit</h2>
         <div className="cards--container">
-          <img src={BlackCard} alt="" />
-          <img src={GoldCard} alt="" />
-          <img src={PlatinumCard} alt="" />
+          <CardsElement card={"GOLD"} />
+          <CardsElement card={"TITANIUM"} />
+          <CardsElement card={"BLACK"} />
         </div>
       </div>
       <div className="my-20 get_new--container">
         <NavLink to="/new-card">
           <button className="get_new_acc">Solicitar una tarjeta nueva</button>
-         </NavLink>
+        </NavLink>
       </div>
     </>
   );
