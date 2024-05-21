5c2ee3a287fde3ae630cebfb9438b08ab63cd823
Lucas Madrigal <lucasmadrigalb@gmail.com>
2024-05-21 02:36:13 -0300

    cards wip 2

--- src/components/CardsElement.jsx
+++ src/components/CardsElement.jsx
@@ -4,18 +4,14 @@ import GoldCard from "../assets/images/imgs/gold_card.png";
 import PlatinumCard from "../assets/images/imgs/platinum_card.png";
 
 const CardsElement = ({ card }) => {
-//   console.log("🚀 ~ CardsElement ~ card:", card);
 
   const Card = () => {
     switch (card) {
       case "GOLD":
-        console.log("🚀 ~ Card ~ GoldCard:", GoldCard);
         return GoldCard;
       case "TITANIUM":
-        console.log("🚀 ~ Card ~ PlatinumCard:", PlatinumCard);
         return PlatinumCard;
       case "BLACK":
-        console.log("🚀 ~ Card ~ BlackCard:", BlackCard);
         return BlackCard;
     }
   };
--- src/pages/Cards.jsx
+++ src/pages/Cards.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../styles/Cards.css";
 import BlackCard from "../assets/images/imgs/black_card.png";
 import GoldCard from "../assets/images/imgs/gold_card.png";
@@ -6,21 +6,41 @@ import PlatinumCard from "../assets/images/imgs/platinum_card.png";
 import { NavLink } from "react-router-dom";
 import CardsElement from "../components/CardsElement";
 const Cards = () => {
+  const [cards, setCards] = useState();
+  const [cardDebit, setCardDebit] = useState([]);
+  const [cardCredit, setCardCredit] = useState([]);
+  const url = "http://localhost:8080/api";
+  useEffect(() => {
+    fetch(`${url}/clients/1`)
+      .then((res) => res.json())
+      .then((data) => {
+        setCards(data.cards);
+        console.log(data);
+      });
+  }, []);
+
+  useEffect(() => {
+    const cardsDebit = cards?.filter((c) => c.type === "DEBIT");
+    const cardsCredit = cards?.filter((c) => c.type === "CREDIT");
+    setCardDebit(cardsDebit);
+    setCardCredit(cardsCredit);
+  }, [cards]);
+
   return (
     <>
       <h1>Your cards</h1>
       <div className="main-cards--container">
         <h2>Credit</h2>
         <div className="cards--container">
-          <CardsElement card={"GOLD"} />
-          <CardsElement card={"TITANIUM"} />
-          <CardsElement card={"BLACK"} />
+          {cardCredit?.map((c, i) => (
+            <CardsElement key={i} card={c.color} />
+          ))}
         </div>
         <h2>Debit</h2>
         <div className="cards--container">
-          <CardsElement card={"GOLD"} />
-          <CardsElement card={"TITANIUM"} />
-          <CardsElement card={"BLACK"} />
+          {cardDebit?.map((c, i) => (
+            <CardsElement key={i} card={c.color} />
+          ))}
         </div>
       </div>
       <div className="my-20 get_new--container">
